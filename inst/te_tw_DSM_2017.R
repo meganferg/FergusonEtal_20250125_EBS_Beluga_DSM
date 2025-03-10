a67e89381acba596e0db4b602457e4da1a274add
Megan Ferguson <Megan.Ferguson@briloon.org>
2025-03-10 10:12:42 -0400

    Finished running through from start to finish and cleaning

--- inst/te_tw_DSM_2017.R
+++ inst/te_tw_DSM_2017.R
@@ -1,4 +1,4 @@
-#Script te_tw_DSM_2017.R...Megan C. Ferguson...27 January 2025
+#Script te_tw_DSM_2017.R...Megan C. Ferguson...10 March 2025
 
   #Notes
   #
@@ -11,13 +11,13 @@
   #     c. log link
   # 
   # 2. Required input files:
-  #    a. Data/FergusonEtal_20250125_EBS_Beluga_DSM_data.Rdata
-  #    b. R/mcf_mod_eval_plots.R
-  #    c. cpp/te_tw_DSM.cpp
-  #    d. cpp/null_tw_DSM.cpp
+  #    a. data/FergusonEtal_20250125_EBS_Beluga_DSM_data.Rdata
+  #    b. inst/mcf_mod_eval_plots.R
+  #    c. src/te_tw_DSM.cpp
+  #    d. src/null_tw_DSM.cpp
   #
   # 3. This script requires the R package TMB. For information on 
-  #    installing TMB, see https://github.com/kaskr/adcomp/wiki/Download
+  #    installing TMB, see https://github.com/kaskinst/adcomp/wiki/Download
   #
   # 4. Figures are output to a folder called "Figures" in the working directory.
   #
@@ -43,11 +43,8 @@
       
     #Input necessary objects
     
-      load("Data/FergusonEtal_20250125_EBS_Beluga_DSM_data.Rdata")
-      source("R/mcf_mod_eval_plots.R")
-      #CK
-        summary(predgrid.strat)
-        summary(gam.dat17)
+      load("data/FergusonEtal_20250125_EBS_Beluga_DSM_data.Rdata")
+      source("inst/mcf_mod_eval_plots.R")
 
     #Create mgcv model
       
@@ -62,7 +59,7 @@
     #which saves plots to the location specified by the fnam argument to 
     #mod.eval.plots; and 2) DHARMa plots
             
-      Fnam <- "Figures/te_tw_DSM_2017"
+      Fnam <- "figures/te_tw_DSM_2017"
             
       mod.eval.plots(m=b, mod.typ="tw", tw.p=NA, fnam=Fnam)
             
@@ -86,13 +83,7 @@
                                 method="REML",
                                 data=gam.dat17,
                                 fit = FALSE)
-      #CK
-        dim(gam_setup$X) #ncol should be k^2 bc the basis dimension of the 
-                         #whole te smooth is the product of the basis dimensions of 
-                         #the marginal smooths.
-              
-        nrow(gam.dat17) #604 observations. 
-              
+
     #Extract penalization matrices.
     #  Because the first (and only) smooth is a bivariate smooth of x and y, 
     #  it has 2 penalty matrices associated with it.
@@ -102,10 +93,7 @@
       #Convert to sparse matrices
         S1 <- as(S_xy_1, "CsparseMatrix")
         S2 <- as(S_xy_2, "CsparseMatrix")
-        #CK
-          class(S1) #"dsCMatrix"
-          class(S2) #"dsCMatrix"
-              
+
       #Extract number of parameters needed to define the smooth
         n.beta <- b$smooth[[1]]$last.para - b$smooth[[1]]$first.para + 1 ## number of params
         n.beta #195
@@ -145,14 +133,11 @@
 
         n.RE <- length(par$beta) #Number of random effects in model
         n.RE
-        #CK
-          dim(data_tmb$X) #should be 195
-          length(par$beta) #should be 195
-           
+
     #Compile and load DLL        
             
-      compile("cpp/te_tw_DSM.cpp")
-      dyn.load(dynlib("cpp/te_tw_DSM"))
+      compile("src/te_tw_DSM.cpp")
+      dyn.load(dynlib("src/te_tw_DSM"))
             
     #Construct objective functions with derivatives based on compiled C++
     #template       
@@ -172,10 +157,6 @@
     #Use epsilon detransformation bias correction algorithm
             
       rep <- sdreport( M, par.fixed=opt.bc$par, bias.correct=TRUE)
-      #CK
-        M$report()
-        rep
-        summary(rep, "report")
 
     #Compare cell-wise predictions (plug-in estimator) from mgcv and TMB. 
 
@@ -192,12 +173,6 @@
       #(plug-in estimate)  
                 
         pred_mgcv <- predgrid.strat$a.p*exp(Lp%*%coef(b)) #log link, with offset
-        #CK
-          summary(pred_tmb) #Predicted plug-in Nhat per cell from TMB
-          summary(pred_mgcv) #Predicted plug-in Nhat per cell from mgcv
-              
-          sum(pred_tmb)  #Predicted plug-in Nhat in study area from TMB
-          sum(pred_mgcv) #Predicted plug-in Nhat in study area from mgcv
 
     #Evaluate TMB model fit           
               
@@ -265,7 +240,7 @@
       R1 = sum(M$report()$devresid^2 )
               
     #Unload DLL for candidate DSM     
-      dyn.unload(dynlib("cpp/te_tw_DSM"))
+      dyn.unload(dynlib("src/te_tw_DSM"))
 
     #Steps for computing PDE for the candidate model: 
     # 1. Fit null model; 
@@ -277,8 +252,8 @@
 
         #Compile .cpp for null model then load DLL 
           
-          compile("cpp/null_tw_DSM.cpp")
-          dyn.load(dynlib("cpp/null_tw_DSM"))
+          compile("src/null_tw_DSM.cpp")
+          dyn.load(dynlib("src/null_tw_DSM"))
 
         #Specify data and parameters
     
@@ -295,10 +270,7 @@
           null.M <- MakeADFun(data=null.data_tmb, 
                          parameters=null.par, 
                          DLL="null_tw_DSM")
-              #CK
-                null.M$report()
-                sum(null.M$report()$devresid^2 )
-  
+
       #Optimize null model
       
         Lower <- -50  #trying to prevent -Inf,Inf bounds resulting in nlminb failure (NaN gradient)
@@ -308,18 +280,13 @@
                                iter.max=1000))
       #2. Compute null deviance
         sum.null.dev.sq <- sum(null.M$report()$devresid^2 )
-        #CK
-          sum.null.dev.sq
-          
+
       #3. Recall that the sum of residual squared deviances from candidate model
       #was computed above
         R1 
 
       #4. Use sum.null.dev.sq and R1 to compute PDE.
         pde <- 1 - R1/sum.null.dev.sq
-        #Compare to mgcv's model
-          summary(b)
-          pde
 
 
               
